049d6812627c4a31b18b7ff5a8dfec9317a03504
Joaquintinsin <phoenix_css@hotmail.com>
2023-08-23 10:15:10 -0300

    Added Ej3, P1. Missing main instructions.

--- algoritmos/c/lista/Ej3.c
+++ algoritmos/c/lista/Ej3.c
@@ -8,8 +8,8 @@
 #include <stdlib.h>
 
 typedef struct TLista{
-	int valor;
-	struct TLista* next;
+  int valor;
+  struct TLista* next;
 } TLista;
 
 float PromedioSinAplazos(TLista *l);
@@ -24,72 +24,72 @@ void InsertarC(TLista *l);
 void MostrarLista(TLista *l);
 
 int main( int argc, char **argv ) {
-	
-	
-	
-	return 0;
+  
+  
+  
+  return 0;
 }
 
 float PromedioConAplazos(TLista *l){
-	float resConAplazo = 0;
-	int cantMaterias = 0;
-	TLista *aux = l;
+  float resConAplazo = 0;
+  int cantMaterias = 0;
+  TLista *aux = l;
 
-	while (aux != NULL){
-		if (aux->valor < 5){
-			resConAplazo += aux->valor;
-		}
-		cantMaterias += 1;
-		aux = aux->next;
-	}
+  while (aux != NULL){
+    if (aux->valor < 5){
+      resConAplazo += aux->valor;
+    }
+    cantMaterias += 1;
+    aux = aux->next;
+  }
 
-	return resConAplazo/cantMaterias;
+  return resConAplazo/cantMaterias;
 }
 
 float PromedioSinAplazos(TLista *l){
-	float resSinAplazo = 0;
-	int cantMaterias = 0;
-	TLista *aux = l;
+  float resSinAplazo = 0;
+  int cantMaterias = 0;
+  TLista *aux = l;
 
-	while (aux != NULL){
-		if (aux->valor >= 5){
-			resSinAplazo += aux->valor;
-		}
-		cantMaterias += 1;
-		aux = aux->next;
-	}
+  while (aux != NULL){
+    if (aux->valor >= 5){
+      resSinAplazo += aux->valor;
+    }
+    cantMaterias += 1;
+    aux = aux->next;
+  }
 
-	return resSinAplazo/cantMaterias;
+  return resSinAplazo/cantMaterias;
 }
 
 // Módulos de manejo de listas, 2022 César Cornejo
 //Crea un nodo
 struct TLista* CrearNodo() {
-	struct TLista * nuevo;
-	nuevo = (struct TLista *) malloc (sizeof(struct TLista *));
-	if (nuevo==NULL){
-	 	exit(EXIT_FAILURE); 
-	}
-	else {
-	  printf("Numero: "); fflush(stdout);
-	  scanf("%d",&nuevo->valor);
-	  nuevo->next = NULL;
-	  return nuevo;
-	}
+  struct TLista * nuevo;
+  nuevo = (struct TLista *) malloc (sizeof(struct TLista *));
+  if (nuevo==NULL){
+     exit(EXIT_FAILURE); 
+  }
+  else {
+    printf("Numero: "); fflush(stdout);
+    scanf("%d",&nuevo->valor);
+    nuevo->next = NULL;
+    return nuevo;
+  }
 }
 
 //Inserta un nodo al comienzo de la lista
 void InsertarC(TLista *l) {
-	struct TLista * nuevo;
-	nuevo = CrearNodo();
-	if (l == NULL) {
-		//primer elemento
-	 	l = nuevo;
-	}
-	else {
-		nuevo->next = l;	 
-		l = nuevo;
-	}
+  struct TLista * nuevo;
+  nuevo = CrearNodo();
+  if (l == NULL) {
+    //primer elemento
+     l = nuevo;
+  }
+  else {
+    nuevo->next = l;	 
+    l = nuevo;
+  }
 }
 
 //Muestra la lista
