9456c04a43264574dd377396c4d2de6f4f3c9c17
Joaquintinsin <phoenix_css@hotmail.com>
2023-11-08 19:54:07 -0300

    Ej1, Ej13, Ej5. ArraySorter more important than the others I think.

--- Practica 4/Ej13/arraySorter/ArraySorter.java
+++ Practica 4/Ej13/arraySorter/ArraySorter.java
@@ -1,5 +1,7 @@
 package arraySorter;
 
+import java.util.Arrays;
+
 /**
 * Provee métodos para ordenar arreglos de objetos comparables.
 * Los algoritmos de ordenamiento provistos por esta clase son:
@@ -25,9 +27,9 @@ public class ArraySorter {
     if (array == null) throw new IllegalArgumentException("El arreglo es null, no se puede ordenar");
     boolean sorted = false;
     int n = array.length; 
-    for (int pass = 1; (pass < n) && !sorted; ++pass) {
+    for (int pass = 1; (pass < n) && !sorted; pass++) {
       sorted = true;
-      for (int index = 0; index < n - pass; ++index) {
+      for (int index = 0; index < n-pass; index++) {
         int nextIndex = index + 1;
         if (array[index].compareTo(array[nextIndex]) > 0) {
           swap(array, index, nextIndex);
@@ -47,67 +49,11 @@ public class ArraySorter {
     //last = indice del ultimo elemento de la parte no ordenada
     for (int last = array.length - 1; last >= 1; last--) {
       //largest = posicion del elemento mas grande
-      int largest = indexOfLargest(array, last + 1);
+      int largest = indexOfLargest(array, last+1);
       swap(array, last, largest);
     }
   }
-
-  /**
-   * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos utilizando Shell Sort.
-   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
-   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
-   */
-  public static <T extends Comparable<? super T>> void shellSort(T[] array) {
-    if (array == null) throw new UnsupportedOperationException("El arreglo a ordenar no puede ser null");
-    
-  }
-
-  /**
-   * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos utilizando Quick Sort.
-   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
-   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
-   */
-  public static <T extends Comparable<? super T>> void quickSort(T[] array, int leftmost, int rightmost) {
-    if (array == null) throw new UnsupportedOperationException("El arreglo a ordenar no puede ser null");
-    
-    if (leftmost < rightmost) {
-      int pivot = (leftmost + rightmost) % 2;
-      quickSort(array, leftmost, part-1);
-      quickSort(array, part+1, rightmost);
-    }
-  }
   
-  /*private static <T extends Comparable<? super T>> int particion(T[] array, int leftmost, int rightmost) {
-    T pivot = array[rightmost];
-    int i = (leftmost - 1);
-    for (int j = leftmost ; j < rightmost ; j++) {
-      if (array[j].compareTo(pivot) == -1) {
-        i++;
-        swap(array, i, j);
-      }
-    }
-    swap(array, i+1, rightmost);
-    return (i+1);
-  }*/
-
-  /**
-   * Ordena un arreglo, usando el orden natural de sus elementos utilizando Merge Sort.
-   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
-   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
-   */
-  public static <T extends Comparable<? super T>> void mergeSort(T[] array) {
-    throw new UnsupportedOperationException("TODO: implementar");    
-  }
-
-  /* (non-Javadoc)
-   * Este método intercambia dos posiciones de un arreglo.
-   */ 
-  private static <T extends Comparable<? super T>> void swap(T[] array, int i, int j) {
-    T temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-
   /* (non-Javadoc)
    * Este método retorna el indice del elemento mas grande. 
    */
@@ -119,5 +65,222 @@ public class ArraySorter {
       }
     }  
     return largest;
+  }
+
+  // Idea: Para cada i determinamos el número de j's menores a
+  // él en el arreglo, y acomodamos a i en el lugar que va.
+  // Necesitamos usar arreglos adicionales para esto.
+  
+  // T(n) = n + n + k pertenece O(n)
+  // primera n: Contar menores o iguales
+  // segunda n: Poner los elementos en su lugar
+  // k:         Contar repetidos
+  // Si el k es muy grande, el algoritmo es ineficiente!
+  
+  // n = Numero más grande a aparecer
+  // k = Long de la lista/cantidad de elementos
+  public static void countingSort(int [] array, int n, int k) {
+    int[] b = new int[n];
+    int[] c = new int[k];
+    for (int i = 0; i < n; i++) {
+      //contamos la cantidad de elementos iguales a array [ i ]
+      c[array[i]] = c[array[i]] + 1;
+    }
+    for (int i = 1; i < k; i++) {
+      //contamos la cantidad de elementos iguales o menores a array [ i ]
+      c[i] = c[i] + c[i-1];
+    }
+    // ponemos cada elemento array [ i ] en su lugar
+    for (int j = n-1; i == 0; j--) {    // Tiene problemas en i == 0, no esta declarada la variable. No se que se habra querido poner
+      b[c[array[j]] - 1] = array[j];
+      c[array[j]] = c[array[j]] - 1;
+    }
+  }
+
+  /**
+   * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos utilizando Shell Sort.
+   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
+   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
+   */
+  // ShellSort de chat
+  public static <T extends Comparable<? super T>> void shellSort(T[] array) {
+    if (array == null) throw new IllegalArgumentException("array is null, can't sort");
+    int n = array.length;
+
+    // Comenzar con un espacio grande y reducirlo gradualmente
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+      // Realizar la inserción de elementos con un cierto espacio (gap)
+      for (int i = gap; i < n; i++) {
+        T temp = array[i];
+        int j;
+        for (j = i; j >= gap && array[j-gap].compareTo(temp) > 0; j -= gap) {
+          array[j] = array[j - gap];
+        }
+        array[j] = temp;
+      }
+    }
+  }
+  
+  // RadixSort de chat
+  public static void radixSort(int[] arr) {
+    if (arr == null || arr.length == 0) throw new IllegalArgumentException("array is null, can't sort");
+
+    int max = Arrays.stream(arr).max().getAsInt();
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+      countingSortByDigit(arr, exp);
+    }
+  }
+
+  // Para el RadixSort
+  private static void countingSortByDigit(int[] arr, int exp) {
+    int n = arr.length;
+    int[] output = new int[n];
+    int[] count = new int[10]; // 10 digits (0-9)
+
+    Arrays.fill(count, 0);
+
+    // Count the occurrences of each digit in the current position
+    for (int i = 0; i < n; i++) {
+      count[(arr[i] / exp) % 10]++;
+    }
+
+    // Calculate the acumulative count
+    for (int i = 1; i < 10; i++) {
+      count[i] += count[i - 1];
+    }
+
+    // Build the output array using the counts and place elements correctly
+    for (int i = n - 1; i >= 0; i--) {
+      output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+      count[(arr[i] / exp) % 10]--;
+    }
+
+    // Copy the sorted elements back to the original array
+    for (int i = 0; i < n; i++) {
+      arr[i] = output[i];
+    }
+  }
+
+  public static <T extends Comparable<? super T>> void insertionSort (T[] array) {
+    int n = array.length;
+    for (int unsorted = 1; unsorted < n; unsorted++) {
+      T nextItem = array[unsorted];
+      int loc = unsorted;
+      while ((loc > 0) && (array[loc-1].compareTo(nextItem) > 0)) {
+        array[loc] = array[loc-1];
+        loc--;
+      }
+      array[loc] = nextItem;
+    }
+  }
+
+  /**
+   * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos utilizando Quick Sort.
+   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
+   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
+   */
+  public static <T extends Comparable<? super T>> void quickSort(T[] array, int leftmost, int rightmost) {
+    if (array == null) throw new IllegalArgumentException("array is null, can't sort");
+    
+    if (leftmost < rightmost) {
+      int p = partition(array, leftmost, rightmost);
+      quickSort(array, leftmost, p);
+      quickSort(array, p+1, rightmost);
+    }
+  }
+  
+  // El partition no necesita espacio extra.
+  // No es un algoritmo estable (depende de la implementación del partition).
+  private static <T extends Comparable<? super T>> int partition(T[] array, int begin, int end) {
+    T pivot = array[begin];
+    int i = begin - 1;
+    int j = end + 1;
+    while (i < j) {
+      //invariante:
+      //para k < = i : a[k] <= pivot y para k >= j : pivot <= a[k]
+      do 
+        j--; 
+      while (array[j].compareTo(pivot) > 0);
+      do 
+        i++; 
+      while (array[i].compareTo(pivot) < 0);
+      if (i < j)
+        swap(array, i, j);
+    }
+    return j;
+  }
+
+  /**
+   * Ordena un arreglo, usando el orden natural de sus elementos utilizando Merge Sort.
+   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
+   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
+   */
+  public static <T extends Comparable<? super T>> void mergeSort(T[] array) {
+    if (array == null) throw new IllegalArgumentException("array is null, can't sort");
+    mergeSortRecursivo(array, 0, array.length);
+  }
+  
+  private static <T extends Comparable<? super T>> void mergeSortRecursivo(T[] array, int begin, int end) {
+    if (begin < end) {
+      int mid = (begin + end)/2;
+      mergeSortRecursivo(array, begin, mid);   // ordena la primera mitad
+      mergeSortRecursivo(array, mid+1, end);   // ordena la segunda mitad
+      merge(array, begin, mid, end);  // mezcla las mitades ordenadas
+    }
+  }
+
+  // merge según Chat
+  private static <T extends Comparable<? super T>> void merge(T[] array, int begin, int mid, int end) {
+    int leftArraySize = mid - begin + 1;
+    int rightArraySize = end - mid;
+
+    // Crear arrays temporales para almacenar las mitades izquierda y derecha
+    T[] leftArray = (T[]) new Object[leftArraySize];
+    T[] rightArray = (T[]) new Object[rightArraySize];
+
+    // Copiar datos a los arrays temporales
+    for (int i = 0; i < leftArraySize; i++) {
+      leftArray[i] = array[begin + i];
+    }
+    for (int j = 0; j < rightArraySize; j++) {
+      rightArray[j] = array[mid + 1 + j];
+    }
+
+    int i = 0, j = 0; // Índices iniciales para recorrer los arrays temporales
+    int k = begin; // Índice inicial del subarray combinado
+
+    // Combinar los elementos en orden en el array original
+    while (i < leftArraySize && j < rightArraySize) {
+      if (leftArray[i].compareTo(rightArray[j]) <= 0) {
+        array[k] = leftArray[i];
+        i++;
+      } else {
+        array[k] = rightArray[j];
+        j++;
+      }
+      k++;
+    }
+
+    // Copiar los elementos restantes de los arrays temporales, si los hay
+    while (i < leftArraySize) {
+      array[k] = leftArray[i];
+      i++;
+      k++;
+    }
+
+    while (j < rightArraySize) {
+      array[k] = rightArray[j];
+      j++;
+      k++;
+    }
+  }
+
+  /* (non-Javadoc)
+   * Este método intercambia dos posiciones de un arreglo.
+   */ 
+  private static <T extends Comparable<? super T>> void swap(T[] array, int i, int j) {
+    T temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
 }
--- Practica 4/Ej5/ListaSobreArreglos.java
+++ Practica 4/Ej5/ListaSobreArreglos.java
@@ -8,115 +8,114 @@ package Ej5;
     que sea O(log N) en otro caso, manteniendo la inserción y la eliminación en O(N).
 */
 public class ListaSobreArreglos<T extends Comparable <T>> {
-    private static final int MAXLIST = 100;
-    private T[] item;
-    private int numItems;
-    
-    public ListaSobreArreglos (){
-        item = (T[]) new Comparable[MAXLIST];
-        numItems = 0;
-    }
-    
-    public ListaSobreArreglos (int capMax) {
-        item = (T[]) new Comparable[capMax];
-        numItems = 0;
-    }
-        
-    public Integer buscar(T x) {
-        if (numItems == 0)
-            throw new ArrayIndexOutOfBoundsException("El arreglo está vacío");
-        if (item[numItems-1].compareTo(x) == 0)
-            return numItems-1;
-        
-        Integer resBusqueda = buscarDicot(0, numItems, x);
-        
-        if (item[resBusqueda].compareTo(x) != 0)
-            throw new ArrayIndexOutOfBoundsException("No se encontró el elemento");
-        
-        return resBusqueda;
-    }
-        
-    private Integer buscarDicot(int x, int y, T elem) {
-        int pivot = (x+y) % 2;
-        
-        if (x < y)
-            return pivot;
-        if (item[pivot].compareTo(elem) == 0)
-            return pivot;
-        if (item[pivot].compareTo(elem) == -1)
-            buscarDicot(x, pivot-1, elem);
-        if (item[pivot].compareTo(elem) == 1)
-            buscarDicot(y, pivot, elem);
-            
-        return pivot; // O se pasan los indices o se encuentra el elemento, no entra nunca a este return
-    }
-        
-    private void corrimiento(int pos) {
-        T aux = item[pos];
-        do{
-            item[pos+1] = aux;
-            aux = item[pos];
-            pos++;
-        } while (pos < numItems);
-    }
-        
-    public void insercion(T x) {
-        if (numItems == 0) {
-            item[numItems] = x;
-            numItems++;
-        }
-        
-        boolean auxBool = true;
-        int i;
-        for (i = 0 ; auxBool && i < numItems && i < MAXLIST ; i++) {
-            if (item[i].compareTo(x) == 0) {
-                corrimiento(i+1);
-                item[i] = x;
-                numItems++;
-                auxBool = false;
-            }else{
-                if (item[i].compareTo(x) == 1) {
-                    corrimiento(i-1);
-                    item[i] = x;
-                    numItems++;
-                    auxBool = false;
-                }
-                // Si el compareTo da -1 entonces sigue buscando
-            }
-        }
-        if (i == MAXLIST)
-            throw new IllegalArgumentException("El arreglo está lleno");
-        
-        if (i == numItems) {
-            item[numItems] = x;
-            numItems++;
-        }
+  private static final int MAXLIST = 100;
+  private T[] item;
+  private int numItems;
+
+  public ListaSobreArreglos (){
+    item = (T[]) new Comparable[MAXLIST];
+    numItems = 0;
+  }
+
+  public ListaSobreArreglos (int capMax) {
+    item = (T[]) new Comparable[capMax];
+    numItems = 0;
+  }
+
+  public Integer buscar(T x) {
+    if (numItems == 0)
+      throw new ArrayIndexOutOfBoundsException("El arreglo está vacío");
+    if (item[numItems-1].compareTo(x) == 0)
+      return numItems-1;
+
+    Integer resBusqueda = buscarDicot(0, numItems, x);
+
+    if (item[resBusqueda].compareTo(x) != 0)
+      throw new ArrayIndexOutOfBoundsException("No se encontró el elemento");
+
+    return resBusqueda;
+  }
+
+  private Integer buscarDicot(int x, int y, T elem) {
+    int pivot = (x+y) % 2;
+
+    if (x < y)
+      return pivot;
+    if (item[pivot].compareTo(elem) == 0)
+      return pivot;
+    if (item[pivot].compareTo(elem) == -1)
+      buscarDicot(x, pivot-1, elem);
+    if (item[pivot].compareTo(elem) == 1)
+      buscarDicot(y, pivot, elem);
+
+    return pivot; // O se pasan los indices o se encuentra el elemento, no entra nunca a este return
+  }
+
+  private void corrimiento(int pos) {
+    T aux = item[pos];
+    do{
+      item[pos+1] = aux;
+      aux = item[pos];
+      pos++;
+    } while (pos < numItems);
+  }
+
+  public void insercion(T x) {
+    if (numItems == 0) {
+      item[numItems] = x;
+      numItems++;
     }
 
-    public void eliminacion(T x) {
-        if (numItems == 0)
-            throw new IllegalArgumentException("El arreglo está vacío");
-        
-        boolean auxBool = true;
-        int i;
-        for (i = 0 ; auxBool && i < numItems && i < MAXLIST ; i++) {
-            if (item[i].compareTo(x) == 0) {
-                corrimiento(i-1);
-                numItems--;
-                auxBool = false;
-            }else{
-                if (item[i].compareTo(x) == 1) {
-                    corrimiento(i+1);
-                    numItems--;
-                    auxBool = false;
-                }
-                // Si el compareTo da -1 entonces sigue buscando
-            }
+    boolean auxBool = true;
+    int i;
+    for (i = 0 ; auxBool && i < numItems && i < MAXLIST ; i++) {
+      if (item[i].compareTo(x) == 0) {
+        corrimiento(i+1);
+        item[i] = x;
+        numItems++;
+        auxBool = false;
+      }else{
+        if (item[i].compareTo(x) == 1) {
+          corrimiento(i-1);
+          item[i] = x;
+          numItems++;
+          auxBool = false;
         }
-        if (i == MAXLIST) {
-            item[i-1] = item[i];
-            numItems--;
-        }
+        // Si el compareTo da -1 entonces sigue buscando
+      }
     }
+    if (i == MAXLIST)
+      throw new IllegalArgumentException("El arreglo está lleno");
+
+    if (i == numItems) {
+      item[numItems] = x;
+      numItems++;
+    }
+  }
+
+  public void eliminacion(T x) {
+    if (numItems == 0)
+      throw new IllegalArgumentException("El arreglo está vacío");
+
+    boolean auxBool = true;
+    int i;
+    for (i = 0 ; auxBool && i < numItems && i < MAXLIST ; i++) {
+      if (item[i].compareTo(x) == 0) {
+        corrimiento(i-1);
+        numItems--;
+        auxBool = false;
+      }else{
+        if (item[i].compareTo(x) == 1) {
+          corrimiento(i+1);
+          numItems--;
+          auxBool = false;
+        }
+        // Si el compareTo da -1 entonces sigue buscando
+      }
+    }
+    if (i == MAXLIST) {
+      item[i-1] = item[i];
+      numItems--;
+    }
+  }
 }
-
