74d97d09faafa806d269aa451863ef514c65cdeb
Joaquin Tissera <phoenix_css@hotmail.com>
2024-06-21 20:47:17 -0300

    Implemented some sorting methods
    Refactored by default vscode indentation for those files

--- algoritmos/java/arraySorter/ArrayGenerator.java
+++ algoritmos/java/arraySorter/ArrayGenerator.java
@@ -3,62 +3,108 @@ package arraySorter;
 import java.util.Random;
 
 /**
-* Genera arreglos de {@code Integer} random de una longitud dada.
-*
-*/
+ * Genera arreglos de {@code Integer} random de una longitud dada.
+ *
+ */
 public final class ArrayGenerator {
 
    /**
-   * La longitud por defecto para generar arreglos aleatorios de enteros.
-   */
+    * La longitud por defecto para generar arreglos aleatorios de enteros.
+    */
    public static final int DEFAULT_LONGITUD = 10;
 
-
    /**
-   * Genera un arreglo de enteros con valores aleatorios.
-   * Este método es equivalente a <pre>generarArreglo({@code min}, {@code max}, {@value #DEFAULT_LONGITUD}, {@code System.currentTimeMillis()})</pre>
-   * @param min el limite inferior del rango de valores a generar
-   * @param max el limite superior del rango de valores a generar, no puede ser menor a {@code min}
-   * @return un arreglo de {@value #DEFAULT_LONGITUD} enteros aleatorios generados a partir de {@code System.currentTimeMillis()}
-   * @see System#currentTimeMillis()
-   */
+    * Genera un arreglo de enteros con valores aleatorios.
+    * Este método es equivalente a
+    * 
+    * <pre>generarArreglo({@code
+    * min
+    * }, {@code
+    * max
+    * }, {@value #DEFAULT_LONGITUD}, {@code
+    * System.currentTimeMillis()
+    * })</pre>
+    * 
+    * @param min el limite inferior del rango de valores a generar
+    * @param max el limite superior del rango de valores a generar, no puede ser
+    *            menor a {@code min}
+    * @return un arreglo de {@value #DEFAULT_LONGITUD} enteros aleatorios generados
+    *         a partir de {@code System.currentTimeMillis()}
+    * @see System#currentTimeMillis()
+    */
    public static Integer[] generarArreglo(int min, int max) {
       return generarArreglo(min, max, DEFAULT_LONGITUD, System.currentTimeMillis());
    }
-   
+
    /**
-   * Genera un arreglo de enteros con valores aleatorios, utilizando una semilla particular.
-   * Este método es equivalente a <pre>generarArreglo({@code min}, {@code max}, {@value #DEFAULT_LONGITUD}, {@code semilla})</pre>
-   * @param min el limite inferior del rango de valores a generar
-   * @param max el limite superior del rango de valores a generar, no puede ser menor a {@code min}
-   * @param semilla la semilla utilizada por el generar aleatorio para la secuencia pseudo aleatoria, la misma semilla va a generar el mismo arreglo
-   * @return un arreglo de {@value #DEFAULT_LONGITUD} enteros aleatorios generados a partir de {@code semilla}
-   */
+    * Genera un arreglo de enteros con valores aleatorios, utilizando una semilla
+    * particular.
+    * Este método es equivalente a
+    * 
+    * <pre>generarArreglo({@code
+    * min
+    * }, {@code
+    * max
+    * }, {@value #DEFAULT_LONGITUD}, {@code
+    * semilla
+    * })</pre>
+    * 
+    * @param min     el limite inferior del rango de valores a generar
+    * @param max     el limite superior del rango de valores a generar, no puede
+    *                ser menor a {@code min}
+    * @param semilla la semilla utilizada por el generar aleatorio para la
+    *                secuencia pseudo aleatoria, la misma semilla va a generar el
+    *                mismo arreglo
+    * @return un arreglo de {@value #DEFAULT_LONGITUD} enteros aleatorios generados
+    *         a partir de {@code semilla}
+    */
    public static Integer[] generarArregloConSemilla(int min, int max, long semilla) {
       return generarArreglo(min, max, DEFAULT_LONGITUD, semilla);
    }
-   
+
    /**
-   * Genera un arreglo de enteros con valores aleatorios, de una longitud particular.
-   * Este método es equivalente a <pre>generarArreglo({@code min}, {@code max}, {@code longitud}, {@code System.currentTimeMillis()})</pre>
-   * @param min el limite inferior del rango de valores a generar
-   * @param max el limite superior del rango de valores a generar, no puede ser menor a {@code min}
-   * @param longitud la cantidad de enteros que va a tener el arreglo, debe ser mayor o igual a {@code 0}
-   * @return un arreglo de {@code longitud} enteros aleatorios generados a partir de {@code System.currentTimeMillis()}
-   * @see System#currentTimeMillis()
-   */
+    * Genera un arreglo de enteros con valores aleatorios, de una longitud
+    * particular.
+    * Este método es equivalente a
+    * 
+    * <pre>generarArreglo({@code
+    * min
+    * }, {@code
+    * max
+    * }, {@code
+    * longitud
+    * }, {@code
+    * System.currentTimeMillis()
+    * })</pre>
+    * 
+    * @param min      el limite inferior del rango de valores a generar
+    * @param max      el limite superior del rango de valores a generar, no puede
+    *                 ser menor a {@code min}
+    * @param longitud la cantidad de enteros que va a tener el arreglo, debe ser
+    *                 mayor o igual a {@code 0}
+    * @return un arreglo de {@code longitud} enteros aleatorios generados a partir
+    *         de {@code System.currentTimeMillis()}
+    * @see System#currentTimeMillis()
+    */
    public static Integer[] generarArregloDeLongitud(int min, int max, int longitud) {
       return generarArreglo(min, max, longitud, System.currentTimeMillis());
    }
-   
+
    /**
-   * Genera un arreglo de enteros con valores aleatorios, de una longitud particular, y utilizando una semilla particular.
-   * @param min el limite inferior del rango de valores a generar
-   * @param max el limite superior del rango de valores a generar, no puede ser menor a {@code min}
-   * @param longitud la cantidad de enteros que va a tener el arreglo, debe ser mayor o igual a {@code 0}
-   * @param semilla la semilla utilizada por el generar aleatorio para la secuencia pseudo aleatoria, la misma semilla va a generar el mismo arreglo
-   * @return un arreglo de {@code longitud} enteros aleatorios generados a partir de {@code semilla}
-   */
+    * Genera un arreglo de enteros con valores aleatorios, de una longitud
+    * particular, y utilizando una semilla particular.
+    * 
+    * @param min      el limite inferior del rango de valores a generar
+    * @param max      el limite superior del rango de valores a generar, no puede
+    *                 ser menor a {@code min}
+    * @param longitud la cantidad de enteros que va a tener el arreglo, debe ser
+    *                 mayor o igual a {@code 0}
+    * @param semilla  la semilla utilizada por el generar aleatorio para la
+    *                 secuencia pseudo aleatoria, la misma semilla va a generar el
+    *                 mismo arreglo
+    * @return un arreglo de {@code longitud} enteros aleatorios generados a partir
+    *         de {@code semilla}
+    */
    public static Integer[] generarArreglo(int min, int max, int longitud, long semilla) {
       if (max < min)
          throw new IllegalArgumentException("El valor de max debe ser mayor o igual al valor de min");
@@ -71,11 +117,9 @@ public final class ArrayGenerator {
       }
       return arreglo;
    }
-   
-   
+
    private static Integer generarValorAleatorio(Random rng, int min, int max) {
       Integer valor = rng.nextInt(max - min + 1);
       return valor;
    }
-
 }
--- algoritmos/java/arraySorter/ArraySorter.java
+++ algoritmos/java/arraySorter/ArraySorter.java
@@ -1,31 +1,37 @@
 package arraySorter;
 
 /**
-* Provee métodos para ordenar arreglos de objetos comparables.
-* Los algoritmos de ordenamiento provistos por esta clase son:
-* <ul>
-* <li>Bubble sort.</li>
-* <li>Selection sort.</li>
-* <li>Shell sort.</li>
-* <li>Quick sort.</li>
-* <li>Merge sort.</li>
-* </ul>
-* El invariante que deben cumplir todos los arreglos {@code array} para ser utilizados como argumentos de los distintos algoritmos de ordenamiento es:
-* <pre>
+ * Provee métodos para ordenar arreglos de objetos comparables.
+ * Los algoritmos de ordenamiento provistos por esta clase son:
+ * <ul>
+ * <li>Bubble sort.</li>
+ * <li>Selection sort.</li>
+ * <li>Shell sort.</li>
+ * <li>Quick sort.</li>
+ * <li>Merge sort.</li>
+ * </ul>
+ * El invariante que deben cumplir todos los arreglos {@code array} para ser
+ * utilizados como argumentos de los distintos algoritmos de ordenamiento es:
+ * 
+ * <pre>
 * array != null &amp;&amp; for (T elem : array) {elem != null}
-* </pre>
-*/
+ * </pre>
+ */
 public class ArraySorter {
 
    /**
-   * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos utilizando Bubble Sort.
-   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
-   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
-   */
+    * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos
+    * utilizando Bubble Sort.
+    * 
+    * @param <T>   el tipo de los elementos del arreglo, los cuales deben ser
+    *              comparables entre sí
+    * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
+    */
    public static <T extends Comparable<? super T>> void bubbleSort(T[] array) {
-      if (array == null) throw new IllegalArgumentException("El arreglo es null, no se puede ordenar");
+      if (array == null)
+         throw new IllegalArgumentException("El arreglo es null, no se puede ordenar");
       boolean sorted = false;
-      int n = array.length; 
+      int n = array.length;
       for (int pass = 1; (pass < n) && !sorted; ++pass) {
          sorted = true;
          for (int index = 0; index < n - pass; ++index) {
@@ -39,70 +45,165 @@ public class ArraySorter {
    }
 
    /**
-   * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos utilizando Selection Sort.
-   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
-   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
-   */
+    * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos
+    * utilizando Selection Sort.
+    * 
+    * @param <T>   el tipo de los elementos del arreglo, los cuales deben ser
+    *              comparables entre sí
+    * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
+    */
    public static <T extends Comparable<? super T>> void selectionSort(T[] array) {
-      if (array == null) throw new IllegalArgumentException("array is null, can't sort");
-      //last = indice del ultimo elemento de la parte no ordenada
+      if (array == null)
+         throw new IllegalArgumentException("array is null, can't sort");
+      // last = indice del ultimo elemento de la parte no ordenada
       for (int last = array.length - 1; last >= 1; last--) {
-         //largest = posicion del elemento mas grande
+         // largest = posicion del elemento mas grande
          int largest = indexOfLargest(array, last + 1);
          swap(array, last, largest);
       }
    }
 
-
    /**
-   * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos utilizando Shell Sort.
-   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
-   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
-   */
+    * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos
+    * utilizando Shell Sort.
+    * 
+    * @param <T>   el tipo de los elementos del arreglo, los cuales deben ser
+    *              comparables entre sí
+    * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
+    */
    public static <T extends Comparable<? super T>> void shellSort(T[] array) {
-      throw new UnsupportedOperationException("TODO: implementar");    
+      if (array == null)
+         throw new IllegalArgumentException("array is null, can't sort");
+      int length = array.length;
+      for (int gap = length / 2; gap > 0; gap /= 2) {
+         for (int i = gap; i < length; i++) {
+            T temp = array[i];
+            int j;
+            for (j = i; j >= gap && array[j - gap].compareTo(temp) > 0; j -= gap) {
+               array[j] = array[j - gap];
+            }
+            array[j] = temp;
+         }
+      }
    }
 
    /**
-   * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos utilizando Quick Sort.
-   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
-   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
-   */
+    * Ordena un arreglo, <i>in place</i>, usando el orden natural de sus elementos
+    * utilizando Quick Sort.
+    * 
+    * @param <T>   el tipo de los elementos del arreglo, los cuales deben ser
+    *              comparables entre sí
+    * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
+    */
    public static <T extends Comparable<? super T>> void quickSort(T[] array) {
-      throw new UnsupportedOperationException("TODO: implementar");    
+      quickSort(array, 0, array.length - 1);
+   }
+
+   private static <T extends Comparable<? super T>> void quickSort(T[] array, int low, int high) {
+      if (low < high) {
+         int pivot = partition(array, low, high);
+         quickSort(array, low, pivot - 1);
+         quickSort(array, pivot + 1, high);
+      }
+   }
+
+   private static <T extends Comparable<? super T>> int partition(T[] array, int low, int high) {
+      T pivot = array[high];
+      int i = (low - 1);
+      for (int j = low; j < high; j++) {
+         if (array[j].compareTo(pivot) <= 0) {
+            i++;
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+         }
+      }
+      T temp = array[i + 1];
+      array[i + 1] = array[high];
+      array[high] = temp;
+
+      return i + 1;
    }
 
    /**
-   * Ordena un arreglo, usando el orden natural de sus elementos utilizando Merge Sort.
-   * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
-   * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
-   */
+    * Ordena un arreglo, usando el orden natural de sus elementos utilizando Merge
+    * Sort.
+    * 
+    * @param <T>   el tipo de los elementos del arreglo, los cuales deben ser
+    *              comparables entre sí
+    * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
+    */
    public static <T extends Comparable<? super T>> void mergeSort(T[] array) {
-      throw new UnsupportedOperationException("TODO: implementar");    
+      mergeSort(array, 0, array.length - 1);
    }
 
+   private static <T extends Comparable<? super T>> void mergeSort(T[] array, int low, int high) {
+      if (low < high) {
+         int mid = (low + high) / 2;
+         mergeSort(array, low, mid);
+         mergeSort(array, mid + 1, high);
+         merge(array, low, mid, high);
+      }
+   }
 
-   /* (non-Javadoc)
-   * Este método intercambia dos posiciones de un arreglo.
-   */ 
+   @SuppressWarnings("unchecked")
+   private static <T extends Comparable<? super T>> void merge(T[] array, int left, int mid, int right) {
+      int n1 = mid - left + 1;
+      int n2 = right - mid;
+      T[] leftArray = (T[]) new Object[n1];
+      T[] rightArray = (T[]) new Object[n2];
+      for (int i = 0; i < n1; ++i) {
+         leftArray[i] = array[left + i];
+      }
+      for (int j = 0; j < n2; ++j) {
+         rightArray[j] = array[mid + 1 + j];
+      }
+      int i = 0;
+      int j = 0;
+      int k = left;
+      while (i < n1 && j < n2) {
+         if (leftArray[i].compareTo(rightArray[j]) <= 0) {
+            array[k] = leftArray[i];
+            i++;
+         } else {
+            array[k] = rightArray[j];
+            j++;
+         }
+         k++;
+      }
+      while (i < n1) {
+         array[k] = leftArray[i];
+         i++;
+         k++;
+      }
+      while (j < n2) {
+         array[k] = rightArray[j];
+         j++;
+         k++;
+      }
+   }
+
+   /*
+    * (non-Javadoc)
+    * Este método intercambia dos posiciones de un arreglo.
+    */
    private static <T extends Comparable<? super T>> void swap(T[] array, int i, int j) {
       T temp = array[i];
       array[i] = array[j];
       array[j] = temp;
    }
 
-   /* (non-Javadoc)
-   * Este método retorna el indice del elemento mas grande. 
-   */
-   private static <T extends Comparable<? super T>> int indexOfLargest(T[] array, int n){
+   /*
+    * (non-Javadoc)
+    * Este método retorna el indice del elemento mas grande.
+    */
+   private static <T extends Comparable<? super T>> int indexOfLargest(T[] array, int n) {
       int largest = 0;
-      for (int i = 1; i < n; i++){
-         if (array[i].compareTo(array[largest]) > 0){
+      for (int i = 1; i < n; i++) {
+         if (array[i].compareTo(array[largest]) > 0) {
             largest = i;
          }
-      }  
+      }
       return largest;
    }
-
-
 }
