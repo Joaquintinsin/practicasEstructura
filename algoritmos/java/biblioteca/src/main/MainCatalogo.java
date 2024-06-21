f5863e2292049aa24a24c40e00ffb246a923e5b6
Joaquin Tissera <phoenix_css@hotmail.com>
2024-06-21 20:51:27 -0300

    Default vscode indentation for Java Files

--- algoritmos/java/biblioteca/src/main/MainCatalogo.java
+++ algoritmos/java/biblioteca/src/main/MainCatalogo.java
@@ -4,63 +4,66 @@ import datos.Libro;
 import catalogo.Catalogo;
 
 /**
-* La clase {@code MainCatalogo} implementa una aplicación simple para mostrar el uso de {@code Catalogo} junto con {@code Libro}.
-* Esta clase no usa parámetros, el comportamiento está completamente "hardcodeado" requiriendo modificiar y recompilar para hacer pruebas.
-* @see catalogo.Catalogo
-* @see datos.Libro
-* @version 1.0
-*/
+ * La clase {@code MainCatalogo} implementa una aplicación simple para mostrar
+ * el uso de {@code Catalogo} junto con {@code Libro}.
+ * Esta clase no usa parámetros, el comportamiento está completamente
+ * "hardcodeado" requiriendo modificiar y recompilar para hacer pruebas.
+ * 
+ * @see catalogo.Catalogo
+ * @see datos.Libro
+ * @version 1.0
+ */
 public class MainCatalogo {
 
 	/**
-	* Experimenta con {@code Catalogo}, agregar libros, buscar, y mostrar el {@code Catalogo}.
-	* @param args : arguments for this main method, not used in this implementation.
-	*/
+	 * Experimenta con {@code Catalogo}, agregar libros, buscar, y mostrar el
+	 * {@code Catalogo}.
+	 * 
+	 * @param args : arguments for this main method, not used in this
+	 *             implementation.
+	 */
 	public static void main(String[] args) {
 		Libro libro1 = new Libro("Isaac Asimov", "The Caves of Steel", 42);
 		Libro libro2 = new Libro("Isaac Asimov", "The Naked Sun", 47);
-		//TODO: agregue más libros
-		
+
 		Catalogo catalogo = new Catalogo(10);
-		//TODO: pruebe los métodos agregarLibro y buscarPorTitulo
 		System.out.println("Catalogo:\n" + catalogo);
-		
+
 		if (catalogo.agregarLibro(libro1))
 			System.out.println("Agregado con exito \n");
 		else
 			System.out.println("No se pudo agregar \n");
-		
+
 		System.out.println("Catalogo:\n" + catalogo);
-		
+
 		if (catalogo.agregarLibro(libro2))
 			System.out.println("Agregado con exito \n");
 		else
 			System.out.println("No se pudo agregar \n");
-		
+
 		System.out.println("Catalogo:\n" + catalogo);
-		
+
 		Libro lookingForLibro = catalogo.buscarPorTitulo("Diccionario");
 		if (lookingForLibro == null)
 			System.out.println("No se encontró el libro \n");
 		else
 			System.out.println(lookingForLibro);
-		
+
 		Libro libro4 = new Libro("Joaquin", "Diccionario", 126);
 		if (catalogo.agregarLibro(libro4))
 			System.out.println("Agregado con exito \n");
 		else
 			System.out.println("No se pudo agregar \n");
-		
+
 		lookingForLibro = catalogo.buscarPorTitulo("Diccionario");
-		
+
 		if (lookingForLibro == null)
 			System.out.println("No se encontró el libro \n");
 		else
 			System.out.println(lookingForLibro);
-		
+
 		boolean varLogica = (libro4 == lookingForLibro);
 		System.out.println(varLogica);
-		
-	} 
 
+	}
 }
--- algoritmos/java/biblioteca/src/main/MainLibro.java
+++ algoritmos/java/biblioteca/src/main/MainLibro.java
@@ -3,18 +3,24 @@ package main;
 import datos.Libro;
 
 /**
-* La clase {@code MainLibro} implementa una aplicación simple para mostrar el uso de {@code Libro}.
-* Esta clase no usa parámetros, el comportamiento está completamente "hardcodeado" requiriendo modificiar y recompilar para hacer pruebas.
-* @see datos.Libro
-* @version 1.0
-*/
+ * La clase {@code MainLibro} implementa una aplicación simple para mostrar el
+ * uso de {@code Libro}.
+ * Esta clase no usa parámetros, el comportamiento está completamente
+ * "hardcodeado" requiriendo modificiar y recompilar para hacer pruebas.
+ * 
+ * @see datos.Libro
+ * @version 1.0
+ */
 public class MainLibro {
 
 	/**
-	* Experimenta con libros, sus métodos y la diferencia entre usar {@code equals()} y {@code ==}.
-	* @param args : arguments for this main method, not used in this implementation.
-	* @see Object#equals(Object)
-	*/
+	 * Experimenta con libros, sus métodos y la diferencia entre usar
+	 * {@code equals()} y {@code ==}.
+	 * 
+	 * @param args : arguments for this main method, not used in this
+	 *             implementation.
+	 * @see Object#equals(Object)
+	 */
 	public static void main(String[] args) {
 		Libro libro1 = new Libro("Isaac Asimov", "The Caves of Steel", 42);
 		Libro libro2 = new Libro("Isaac Asimov", "The Caves of Steel", 42);
@@ -23,13 +29,12 @@ public class MainLibro {
 		System.out.println(libro1 + " y " + libro2 + "\nSon iguales usando ==? " + (libro1 == libro2));
 		System.out.println(libro1 + " y " + libro3 + "\nSon iguales usando equals? " + (libro1.equals(libro3)));
 		System.out.println(libro1 + " y " + libro3 + "\nSon iguales usando ==? " + (libro1 == libro3));
-		//TODO: realice más pruebas
-		
+
 		Libro libro4 = new Libro("Joaquin", "Test1", 1);
-		System.out.println("El libro de " + libro4.autor() + " se llama " + libro4.titulo() + " y tiene " + libro4.nroPaginas() + " paginas.");
-		
+		System.out.println("El libro de " + libro4.autor() + " se llama " + libro4.titulo() + " y tiene "
+				+ libro4.nroPaginas() + " paginas.");
+
 		boolean varLogica = (libro4.equals(libro4));
 		System.out.println("Res varLogica: " + varLogica);
 	}
-
 }
