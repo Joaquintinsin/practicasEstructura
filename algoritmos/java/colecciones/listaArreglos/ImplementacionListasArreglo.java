7fcbd256058f5f880ef449745a385efb1eaf1774
Joaquin Tissera <phoenix_css@hotmail.com>
2024-06-22 00:19:55 -0300

    Implemented Lists with an Array

--- algoritmos/java/colecciones/listaArreglos/ImplementacionListasArreglo.java
+++ algoritmos/java/colecciones/listaArreglos/ImplementacionListasArreglo.java
@@ -4,139 +4,169 @@ public class ImplementacionListasArreglo<T> implements Lista<T> {
     public static final int CAP_MAX_ARREGLO = 100;
     private T[] arr;
     private int cantElem;
-    
-    public ImplementacionListasArreglo(){
-        this(CAP_MAX_ARREGLO);
+    private boolean instance = false; // Flag de instanciamiento, para el repOK
+
+    @SuppressWarnings("unchecked")
+    public ImplementacionListasArreglo() {
+        arr = (T[]) new Object[CAP_MAX_ARREGLO];
+        cantElem = 0;
+        instance = true;
     }
-    
-    public ImplementacionListasArreglo(int cantidad){
+
+    @SuppressWarnings("unchecked")
+    public ImplementacionListasArreglo(int cantidad) {
         arr = (T[]) new Object[cantidad];
         cantElem = 0;
+        instance = true;
     }
-    
-    public boolean agregar(T elem){
+
+    /** {@inheritDoc} */
+    public boolean agregar(T elem) {
         // no quiero que venga null ni que venga este mismo objeto
-        if (elem == null || elem == this || cantElem >= CAP_MAX_ARREGLO-1)
+        if (elem == null || elem == this || cantElem >= CAP_MAX_ARREGLO - 1)
             return false;
         arr[cantElem] = elem;
         cantElem++;
         return true;
     }
-    
-    public boolean agregarTodos(Lista<T> otraLista){
-        if ( arr.length == cantElem ){
+
+    /** {@inheritDoc} */
+    public boolean agregarTodos(Lista<T> otraLista) {
+        // Si esta lista ya esta llena, no puede agregar mas nada
+        if (arr.length == cantElem)
             return false;
-        }
-        
-        for ( int i = 0 ; cantElem < arr.length && i < arr.length ; i++){
+        // Si la cantidad de elementos de las dos listas supera la capacidad maxima
+        // actual, no puede agregar mas nada
+        if (cantElem + otraLista.elementos() >= arr.length)
+            return false;
+
+        // Agrega al final de este arreglo hasta que se llene o hasta terminar los
+        // elementos de la otra lista. Lo que ocurra primero
+        for (int i = 0; cantElem < arr.length && i < otraLista.elementos(); i++) {
             arr[cantElem] = otraLista.obtener(i);
             cantElem++;
         }
-        
+
+        // Devuelve la comparacion de:
+        // Sumar la cantidad de elementos de las dos listas, si supero el arr.length
+        // entonces es false (no pudo agregar todos)
+        // O si no lo supero entonces pudo agregarlos a todos y devuelve verdadero.
         return (cantElem + otraLista.elementos()) <= arr.length;
     }
-    
-    public boolean insertar(T elem, int indice){
-        if ( indice < 0 || indice > cantElem ) throw new IndexOutOfBoundsException("Indice fuera de rango");
-        
-        if ( arr.length == cantElem )
+
+    /** {@inheritDoc} */
+    public boolean insertar(T elem, int indice) {
+        if (indice < 0 || indice >= cantElem)
+            throw new IndexOutOfBoundsException("Indice fuera de rango");
+
+        if (arr.length == cantElem)
             return false;
-        
-        if ( indice == cantElem ){
-            arr[indice] = elem;
-            cantElem++;
-        }else{
-            T aux = arr[indice];
-            arr[indice] = elem;
-            for ( int i = indice ; i < arr.length ; i++ ){
-                arr[i] = aux;
-                aux = arr[i+1];
-            }
-            cantElem++;
+
+        for (int i = cantElem - 1; i > indice; i--) {
+            arr[i] = arr[i - 1];
         }
+        arr[indice] = elem;
+        cantElem++;
         return true;
     }
-    
-    public T eliminar(int indice){
-        if ( indice < 0 || indice > cantElem ) throw new IndexOutOfBoundsException("Indice fuera de rango");
-        
-        T rescueElem = arr[indice-1];
-        arr[indice-1] = null;
-        
-        for (int i = cantElem ; i < indice ; i--)
-            arr[i-1] = arr[i];
+
+    /** {@inheritDoc} */
+    public T eliminar(int indice) {
+        if (indice < 0 || indice >= cantElem)
+            throw new IndexOutOfBoundsException("Indice fuera de rango");
+        T rescueElem = arr[indice];
+
+        for (int i = indice; i < cantElem - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[cantElem - 1] = null;
+        cantElem--;
         return rescueElem;
     }
-    
-    public T obtener(int indice){
+
+    /** {@inheritDoc} */
+    public T obtener(int indice) {
         return arr[indice];
     }
-    
-    public Lista<T> subLista(int desdeInd, int hastaInd){
-        if ( desdeInd < 0 || hastaInd < cantElem || desdeInd > hastaInd) throw new IndexOutOfBoundsException("Indice fuera de rango");
-        
-        Lista<T> listaResultado = new ImplementacionListasArreglo();
-        if ( desdeInd == hastaInd || esVacia() )
+
+    /** {@inheritDoc} */
+    public Lista<T> subLista(int desdeInd, int hastaInd) {
+        if (desdeInd < 0 || hastaInd > cantElem || desdeInd > hastaInd)
+            throw new IndexOutOfBoundsException("Indice fuera de rango");
+
+        Lista<T> listaResultado = new ImplementacionListasArreglo<>();
+        if (desdeInd == hastaInd || esVacia())
             return listaResultado;
-        
-        int i = 0;
-        while ( desdeInd < hastaInd ){
-            listaResultado.insertar(arr[desdeInd], i);
+
+        while (desdeInd < hastaInd) {
+            listaResultado.agregar(arr[desdeInd]);
             desdeInd++;
-            i++;
         }
-        
+
         return listaResultado;
     }
-    
-    public boolean contiene(T elem){
-        for ( int i = 0 ; i < cantElem ; i++ ){
-            if ( arr[i] == elem )
+
+    /** {@inheritDoc} */
+    public boolean contiene(T elem) {
+        for (int i = 0; i < cantElem; i++) {
+            if (arr[i] == elem)
                 return true;
         }
         return false;
     }
-    
-    public void vaciar(){
+
+    /** {@inheritDoc} */
+    public void vaciar() {
         cantElem = 0;
     }
-    
-    public int elementos(){
+
+    /** {@inheritDoc} */
+    public int elementos() {
         return cantElem;
     }
-    
-    public boolean esVacia(){
-        return ( cantElem == 0 );
+
+    /** {@inheritDoc} */
+    public boolean esVacia() {
+        return (cantElem == 0);
     }
-    
-    public boolean repOK(){
-        throw new UnsupportedOperationException("Debe implementar este método");
+
+    /** {@inheritDoc} */
+    public boolean repOK() {
+        // Si fue instanciado, la cantElem == arr.length. Si no, cumple con el repOK.
+        return (instance) ? cantElem == arr.length : true;
     }
-    
+
+    /** {@inheritDoc} */
     @Override
-	public String toString(){
+    public String toString() {
+        if (esVacia())
+            return "La lista esta vacia";
+
         String msg = "Elementos de la lista implementada con arreglos: \n";
         msg += "Hay " + cantElem + " elementos almacenados en la lista. \n";
-        for ( int i = 0 ; i < cantElem ; i++ ){
-            msg += "Elemento número " + (i+1);
-            msg += arr[i].toString();
+        for (int i = 0; i < cantElem; i++) {
+            msg += "Elemento número " + (i + 1) + ": ";
+            msg += String.valueOf(arr[i]) + "\n";
         }
         return msg;
     }
-    
+
+    /** {@inheritDoc} */
+    @SuppressWarnings("unchecked")
     @Override
-	public boolean equals(Object otro){
-        if ( otro == null || !(otro instanceof ImplementacionListasArreglo) )
+    public boolean equals(Object otro) {
+        if (otro == null || !(otro instanceof ImplementacionListasArreglo))
             return false;
-        
-        ImplementacionListasArreglo otraLista = (ImplementacionListasArreglo) otro;
-        if ( otraLista.elementos() != cantElem )
+
+        ImplementacionListasArreglo<T> otraLista = (ImplementacionListasArreglo<T>) otro;
+        if (otraLista.elementos() != cantElem)
             return false;
-        
+
         boolean logicAux = true;
-        for ( int i = 0 ; i < otraLista.elementos() ; i++ )
+        for (int i = 0; i < otraLista.elementos(); i++)
             logicAux = logicAux && (arr[i] == otraLista.obtener(i));
-        
+
         return logicAux;
     }
 }
--- algoritmos/java/colecciones/listaArreglos/MainListaArreglo.java
+++ algoritmos/java/colecciones/listaArreglos/MainListaArreglo.java
@@ -1,11 +1,52 @@
 package colecciones.listaArreglos;
 
-import colecciones.listaArreglos.ImplementacionListasArreglo;
-import colecciones.listaArreglos.Lista;
+public class MainListaArreglo {
+    public static void main(String[] args) {
+        Lista<Integer> varEnteros = new ImplementacionListasArreglo<Integer>();
+        Lista<Integer> lista2enteros = new ImplementacionListasArreglo<Integer>();
 
-public class MainListaArreglo{
-    public static void main(String[] args){
-        Lista <Integer> varEnteros = new ImplementacionListasArreglo();
-        System.out.println("Hello world");
+        System.out.println(varEnteros.toString());
+
+        boolean resAgregar = varEnteros.agregar(0);
+        varEnteros.agregar(1);
+        varEnteros.agregar(2);
+        varEnteros.agregar(3);
+        varEnteros.agregar(4);
+        System.out.println("resAgregar: " + resAgregar);
+
+        boolean resAgregarTodos = varEnteros.agregarTodos(lista2enteros);
+        System.out.println("resAgregarTodos: " + resAgregarTodos);
+
+        boolean resInsertar = varEnteros.insertar(0, 1);
+        System.out.println("resInsertar: " + resInsertar);
+
+        Integer elemEliminado = varEnteros.eliminar(0);
+        System.out.println("elemEliminado: " + elemEliminado);
+
+        System.out.println(varEnteros.toString());
+
+        Integer elemObtenido = varEnteros.obtener(0);
+        System.out.println("elemObtenido: " + elemObtenido);
+
+        Lista<Integer> sublista = varEnteros.subLista(0, 1);
+        System.out.println("sublista: " + sublista);
+
+        boolean resContiene = varEnteros.contiene(elemObtenido);
+        System.out.println("resContiene: " + resContiene);
+
+        varEnteros.vaciar();
+        System.out.println("desps de vaciar(): " + varEnteros.toString());
+
+        int resElementos = varEnteros.elementos();
+        System.out.println("resElementos: " + resElementos);
+
+        boolean resVacia = varEnteros.esVacia();
+        System.out.println("resVacia: " + resVacia);
+
+        boolean repOk = varEnteros.repOK();
+        System.out.println("repOk: " + repOk);
+
+        boolean eq = varEnteros.equals(lista2enteros);
+        System.out.println("eq: " + eq);
     }
 }
